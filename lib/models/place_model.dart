5f3a59d3feb71970cd511068c219d233e5242f24
Visun517 <visunpraboda999@gmail.com>
2026-04-06 23:47:54 +0530

    add-bottom-navigation-bar

--- lib/models/place_model.dart
+++ lib/models/place_model.dart
@@ -3,14 +3,15 @@ class Place {
   final String  location;
   final String image;
   final double rating;
+  final String about;
 
-  Place({required this.name, required this.location, required this.image, required this.rating});
+  Place({required this.name, required this.location, required this.image, required this.rating, required this.about});
 }
 
 List<Place> popularPlaces = [
-  Place(name: "Bali, Indonesia", location: "Southeast Asia", image: "assets/images/bali.png", rating: 4.9),
-  Place(name: "Maldives", location: "Indian Ocean", image: "assets/images/maldives.png", rating: 4.7),
-  Place(name: "Paris, France", location: "Europe", image: "assets/images/paris.png", rating: 4.8),
-  Place(name: "Machu Picchu, Peru", location: "South America", image: "assets/images/peru.png", rating: 4.8),
-  Place(name: "Tokyo, Japan", location: "East Asia", image: "assets/images/tokyo.png", rating: 4.8),
+  Place(name: "Bali, Indonesia", location: "Southeast Asia", image: "assets/images/bali.png", rating: 4.9, about: "Tropical paradise with stunning beaches and rich culture."),
+  Place(name: "Maldives", location: "Indian Ocean", image: "assets/images/maldives.png", rating: 4.7, about: "Overwater bungalows and crystal-clear waters."),
+  Place(name: "Paris, France", location: "Europe", image: "assets/images/paris.png", rating: 4.8, about: "City of lights with iconic landmarks and world-class cuisine."),
+  Place(name: "Machu Picchu, Peru", location: "South America", image: "assets/images/peru.png", rating: 4.8, about: "Ancient Incan citadel high in the Andes Mountains."),
+  Place(name: "Tokyo, Japan", location: "East Asia", image: "assets/images/tokyo.png", rating: 4.8, about: "Vibrant metropolis blending traditional and modern culture."),
 ];
